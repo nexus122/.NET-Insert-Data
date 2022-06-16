f6c9f6e885dcff2180d691bf637c1ad7a22d1da0
nexus122 <juan-pablo122@hotmail.es>
2022-06-16 09:53:19 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("¿Cual Es tu nombre?");
+
+var name = Console.ReadLine();
+var currentDate = DateTime.Now;
+
+// {Environment.NewLine} Esto genera una nueva linea en la consula!
+Console.WriteLine($"{Environment.NewLine} {currentDate:d} - {currentDate:t} -> Saludos, {name}!");
+Console.Write($"{Environment.NewLine}Press any key to exit...");
+Console.ReadKey(true);
